e50c21a0b1ba5a6f64fbbbf8fda1c925a158e74b
Johann Deng <dengliang2005@hotmail.com>
2019-01-19 12:38:21 +0800

    Update create_edit_blog_spec.rb

--- create_edit_blog_spec.rb
+++ create_edit_blog_spec.rb
@@ -5,22 +5,24 @@ Capybara.default_driver = :selenium # :selenium_chrome and :selenium_chrome_head
 describe "create and edit blog", type: :feature do
 
   before :each do
-    visit('http://localhost:3000/articles')
+    #Open the homepage of blog
+    visit('http://localhost:3000/articles') 
   end
-
+  #This is test case for creating blog
   it "creates a new blog" do
-      click_link 'New Article'
-      fill_in 'Title', with: 'test001'
-      fill_in 'Text', with: 'text001'  
-      click_button 'Create Article'
+      click_link 'New Article' #Click button to create a new blog
+      fill_in 'Title', with: 'test001' #Input title of a blog
+      fill_in 'Text', with: 'text001'  #Input body text of a blog
+      click_button 'Create Article'  #Save this blog
       expect(page).to have_content 'successfully created' #Verify the new blog has been created successfully
   end
 
+  #This is test case for editing blog
   it "edits an existing blog" do
-     click_link 'Edit'
-     fill_in 'Title', with: 'test002'
-     fill_in 'Text', with: 'text002'  
-     click_button 'Update Article'
+     click_link 'Edit' #Click button to eidt a blog
+     fill_in 'Title', with: 'test002' #Update the title
+     fill_in 'Text', with: 'text002'  #Update the body text
+     click_button 'Update Article' #Click button to update this blog
      expect(page).to have_content 'successfully updated' #Verify the new blog has been updated successfully
   end  
 
